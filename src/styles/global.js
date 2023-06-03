25f766c71457c4feb595c1034d0f1f7a03894374
Sofia Neo <sofineo@gmail.com>
2023-06-02 23:30:54 -0300

    Configuração de Estilo Global e tema

---
+++ src/styles/global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+import theme from './theme'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS_DARK.DARK_400};
+    color: ${({ theme }) => theme.COLORS_LIGHT.LIGHT_100}
+  }
+
+  body, input, button {
+    font-size: 1.6rem;
+    outline: none;
+    font-family: 'Roboto', sans-serif;
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.8);
+  }
+  
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,132 @@
+export default {
+  COLORS_LIGHT: {
+    LIGHT_100: '#FFFFFF',
+    LIGHT_200: '#FFFAF1',
+    LIGHT_300: '#E1E1E6',
+    LIGHT_400: '#C4C4CC',
+    LIGHT_500: '#7C7C8A',
+    LIGHT_600: '#76797B',
+    LIGHT_700: '#4D585E'
+  },
+  COLORS_DARK: {
+    DARK_100: '#000405',
+    DARK_200: '#00070A',
+    DARK_300: '#000204',
+    DARK_400: '#000A0F',
+    DARK_500: '#000C12',
+    DARK_600: '#00111A',
+    DARK_700: '#001119',
+    DARK_800: '#0D161B',
+    DARK_900: '#0D1D25',
+    DARK_1000: '#192227'
+  },
+  COLORS_TINTS: {
+    TOMATO_100: '#750310',
+    TOMATO_200: '#92000E',
+    TOMATO_300: '#AB222E',
+    TOMATO_400: '#AB4D55',
+
+    CARROT_100: '#FBA94C',
+
+    MINT_100: '#04D361',
+
+
+    CAKE_100: '#065E7C',
+    CAKE_200: '#82F3FF'
+  },
+  POPPINS: {
+    POPPINS_100_MEDIUM: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Medium',
+      SIZE: '1.4rem',
+      LINE_HEIGHT: '2.4rem',
+    },
+    POPPINS_200_MEDIUM: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Medium',
+      SIZE: '2rem',
+      LINE_HEIGHT: '160%',
+    },
+    POPPINS_300_BOLD: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Bold',
+      SIZE: '2.4rem',
+      LINE_HEIGHT: '140%',
+    },
+    POPPINS_300_REGULAR: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Regular',
+      SIZE: '2.4rem',
+      LINE_HEIGHT: '140%',
+    },
+    POPPINS_400_MEDIUM: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Medium',
+      SIZE: '3.2rem',
+      LINE_HEIGHT: '140%',
+    },
+    POPPINS_500_MEDIUM: {
+      FONT: '"Poppins"',
+      WEIGHT: 'Medium',
+      SIZE: '4rem',
+      LINE_HEIGHT: '140%',
+    },
+
+  },
+  ROBOTO: {
+    ROBOTO_SMALLEST_REGULAR: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Regular',
+      SIZE: '1.2rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_SMALLER_REGULAR: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Regular',
+      SIZE: '1.4rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_SMALLER_BOLD: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Bold',
+      SIZE: '1.4rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_SMALL_SPACED: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Regular',
+      SIZE: '1.6rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_SMALL_REGULAR: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Regular',
+      SIZE: '1.6rem',
+      LINE_HEIGHT: '100%',
+    },
+    ROBOTO_BIG_BOLD: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Bold',
+      SIZE: '2rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_BIGGER_BOLD: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Bold',
+      SIZE: '2.4rem',
+      LINE_HEIGHT: 'Automatic',
+    },
+    ROBOTO_BIGGER_REGULAR: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Regular',
+      SIZE: '3.2rem',
+      LINE_HEIGHT: '160%',
+    },
+    ROBOTO_GIANT_BOLD: {
+      FONT: '"Roboto"',
+      WEIGHT: 'Bold',
+      SIZE: '4.2rem',
+      LINE_HEIGHT: 'Automatic',
+    }
+  }
+}
